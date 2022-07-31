c66d1ecf6de802e85a87b622342ee8f59b9ab7dd
akash Khekade <akashkhekade.sitcomp@gmail.com>
2022-07-31 11:01:52 +0530

    create this repository on this day 30-07-2022

---
+++ BinaryOperator/src/javaBinary/Demo1.java
@@ -0,0 +1,18 @@
+package javaBinary;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+// Arithmatic Operator
+		// this operator generate result int the form of number
+		int num1 =20 , num2 = 30;
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 % num2);
+		
+		
+	}
+
+}
---
+++ BinaryOperator/src/javaBinary/Demo3.java
@@ -0,0 +1,15 @@
+package javaBinary;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// Assignment Operator
+		//these operator will generate result in the form of number
+		int num1 = 20 , num3;
+		System.out.println(num1 += num2);
+		
+		
+
+	}
+
+}
